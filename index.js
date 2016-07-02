a6df0c5c403ee07c5150f5c08f445cbcffe8d2b5
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2016-07-02 13:26:16 +0300

    Write app draft

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+var npm = new require('npm-api')();
+var repo = npm.repo('gulp')
+
+app.get('/', function (req, res) {
+    repo.package()
+    .then(function(pkg) {
+        res.send('https://'+pkg.repository.url.split('//')[1].replace(/\.git$/, ''));
+    })
+    .catch(function(err) {
+        res.status(500).send(err);
+    });
+});
+
+app.listen(80, function () {
+    console.log('Example app listening on port 80!');
+});
+
