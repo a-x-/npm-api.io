3e3e5e3307e2c8fb7e4ea4d97615efbd850b65b2
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2016-07-02 21:36:57 +0300

    Add logging

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 var express = require('express');
 var app = express();
 var npm = new require('npm-api')();
+
 var fs = require('fs');
 var read = function(name) {return fs.readFileSync(name, { encoding: 'utf-8' })};
 var help = read('index.html');
 
-/*var MongoClient = require('mongodb').MongoClient;
-var url = process.env.MONGODB_URI;
-MongoClient.connect(url, function(err, db) {
-  // Use the admin database for the operation
-  var adminDb = db.admin();
-  // List all the available databases
-  adminDb.listDatabases(function(err, dbs) {
-    console.log('dbs.databases ' , dbs.databases);
-    db.close();
-  });
-});*/
+var MongoClient = require('mongodb').MongoClient;
+var mongoUrl = process.env.MONGODB_URI;
 
 app.use(express.static('.'));
 
@@ -24,14 +16,12 @@ app.get('/', function (req, res) {
 });
 
 app.get('/p/:package', function (req, res) {
-    getPackageUrl(req.params.package)
-    .then(function(url) { res.send(url); })
+    getPackageUrl(req.params.package, function(url) { res.send(url); })
     .catch(function(err) { res.status(500).send(err); });
 });
 
 app.get('/go/:package', function (req, res) {
-    getPackageUrl(req.params.package)
-    .then(function(url) { res.redirect(url); })
+    getPackageUrl(req.params.package, function(url) { res.redirect(url); })
     .catch(function(err) { res.status(500).send(err); });
 });
 
@@ -39,20 +29,38 @@ app.listen(process.env.PORT, function () {
     console.log('Example app listening on port ' + process.env.PORT);
 });
 
-function getPackageUrl(name) {
+function getPackageUrl(name, cb) {
     var repo = npm.repo(name);
 
-    // writeDB('packages.log');
-
-    return repo.package()
+    var promise = repo.package()
     .then(function(pkg) {
         var hostpath = pkg.repository.url
         .replace(/^.+\/\//, '')
         .replace(/^.+@/g, '')
         .replace(/:/, '/')
         .replace(/\.git$/, '');
+        var url = 'https://' + hostpath;
 
-        return 'https://' + hostpath;
+        cb(url);
+        return url;
     });
+    
+    writeLogDb(name);
+    return promise;
 }
 
+function writeLogDb(name) {
+    MongoClient.connect(mongoUrl, function(err, db) {
+        if (err) console.log('mongo err', err);
+        if (!db) return;
+        var collection = db.collection('heroku_rc00k60z');
+        collection.insertOne({"package": name}, function(err, result) {
+            // console.log('insert', err, result);
+            // collection.find({}).toArray(function(err, docs) {
+            //     console.log('wtf', docs);
+            //     db.close();
+            // });
+            db.close();
+        });
+    });
+}
